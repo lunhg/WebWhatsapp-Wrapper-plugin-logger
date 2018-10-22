7c8c6b93027f12d622ef6124c6a1d1df9bd40038
lunhg <hacker@dev.org.br>
2018-10-22 13:41:23 +0000

    adicionados primeiros arquivos

---
+++ README.md
@@ -0,0 +1,11 @@
+# Logger 
+Plugin utility for [lunhg/WebWhatsapp-Wrapper](https://www.github.com/lunhg/WebWhatsapp-Wrapper_
+
+# Usage
+
+```python
+import sys
+sys.path.append('<path-to-repo>')
+from logger import Logger 
+log = Logger(logdir=</logdir>)
+```
---
+++ logger.py
@@ -0,0 +1,33 @@
+# Logger utility for lunhg/WebWhatsapp-Wrapper
+# Usage
+# logger = Logger(logdir=</logdir>)
+class Logger(object):
+
+  def __init__(self):
+
+    # Wheres log
+    self.logdir = kwargs.logdir
+    __log__ = "%s_log.json" % self.logdir
+    log = os.path.join(logdir, __log__)
+    self.logfh = open(log, "a")
+    if (self.logfh.tell() == 0):
+      self.logfh.write('[]')
+      self.logfh.flush()
+
+   # Implement a handle function to be called on
+   # lunhg/Webwhatsapp-Wrapper
+   def handle(self, chat, message):
+    msg = {
+      'uid': chat.id,
+      'msg': message.content,
+      'tim': int(time.time()),
+    }
+    pos = self.logfh.tell()
+    self.logfh.seek(pos-1)
+    self.logfh.truncate()
+    if (pos > 2):
+      self.logfh.write(', ')
+      self.logfh.write(json.dumps(msg))
+      self.logfh.write(']')
+      self.logfh.flush()
+   
