0981b61692faf10b6d2daa37bf5586fd21a4eaff
lunhg <hacker@dev.org.br>
2018-10-22 19:56:11 +0000

    added driver to class

--- logger.py
+++ logger.py
@@ -7,11 +7,12 @@ import json
 class Logger(object):
 
   def __init__(self, **kwargs):
+    self.driver = kwargs.get('driver')
 
+  def setup(self, **kwargs):
     # Wheres log
-    self.logdir = kwargs.logdir
-    __log__ = "%s_log.json" % self.logdir
-    log = os.path.join(logdir, __log__)
+    self.logdir = kwargs.get('logdir')
+    log = os.path.join(self.logdir, 'log.json')
     self.logfh = open(log, "a")
     if (self.logfh.tell() == 0):
       self.logfh.write('[]')
@@ -20,17 +21,16 @@ class Logger(object):
    # Implement a handle function to be called on
    # lunhg/Webwhatsapp-Wrapper
    def handle(self, chat, message):
-    msg = {
-      'uid': chat.id,
-      'msg': message.content,
-      'tim': int(time.time()),
-    }
-    pos = self.logfh.tell()
-    self.logfh.seek(pos-1)
-    self.logfh.truncate()
-    if (pos > 2):
-      self.logfh.write(', ')
-      self.logfh.write(json.dumps(msg))
-      self.logfh.write(']')
-      self.logfh.flush()
-   
+     msg = {
+       'uid': chat.id,
+       'msg': message.content,
+       'tim': int(time.time()),
+     }
+     pos = self.logfh.tell()
+     self.logfh.seek(pos-1)
+     self.logfh.truncate()
+     if (pos > 2):
+       self.logfh.write(', ')
+       self.logfh.write(json.dumps(msg))
+       self.logfh.write(']')
+       self.logfh.flush()
